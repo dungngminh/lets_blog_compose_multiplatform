f62ba8755492327a77355727903d92967e4d51a7
dungngminh <63831488+dungngminh@users.noreply.github.com>
2024-12-13 00:15:46 +0700

    define route and navigation

--- composeApp/src/commonMain/kotlin/me/dungngminh/lets_blog_kmp/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/me/dungngminh/lets_blog_kmp/navigation/AppNavigation.kt
@@ -1,53 +1,110 @@
 package me.dungngminh.lets_blog_kmp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
+import letsblogkmp.composeapp.generated.resources.Res
+import letsblogkmp.composeapp.generated.resources.lets_blog_title
+import me.dungngminh.lets_blog_kmp.presentation.main.favorite.FavoriteScreen
+import me.dungngminh.lets_blog_kmp.presentation.main.home.HomeScreen
+import me.dungngminh.lets_blog_kmp.presentation.main.profile.ProfileScreen
+import me.dungngminh.lets_blog_kmp.presentation.main.search.SearchScreen
 import me.dungngminh.lets_blog_kmp.presentation.onboarding.OnboardingScreen
 import me.dungngminh.lets_blog_kmp.presentation.splash.SplashScreen
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
 
-@Serializable
-object Splash
+sealed interface Route {
+    @Serializable
+    data object Splash : Route
 
-@Serializable
-object Onboarding
+    @Serializable
+    data object Onboarding : Route
 
-@Serializable
-object Main
+    @Serializable
+    data object Main : Route
 
-@Serializable
-object Home
+    @Serializable
+    data object Home : Route
 
-@Serializable
-object Search
+    @Serializable
+    data object Search : Route
 
-@Serializable
-object Favorite
+    @Serializable
+    data object Favorite : Route
 
-@Serializable
-object Profile
+    @Serializable
+    data object Profile : Route
+
+    @Serializable
+    data object Auth : Route
+
+    @Serializable
+    data object SignIn : Route
+
+    @Serializable
+    data object SignUp : Route
+}
+
+enum class MainScreenDestination(
+    val title: StringResource,
+    val icon: DrawableResource,
+    val selectedIcon: DrawableResource,
+    val route: Route,
+) {
+    Home(title = Res.string.lets_blog_title, icon = Res.drawable, route = Route.Home),
+    Search(title = Res.string.lets_blog_title, icon = Res.drawable.email, route = Route.Search),
+    Favorite(title = Res.string.lets_blog_title, icon = Res.drawable.email, route = Route.Favorite),
+    Profile(title = Res.string.lets_blog_title, icon = Res.drawable.email, route = Route.Profile),
+}
 
 @Composable
 fun AppNavigation(navController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navController,
-        startDestination = Splash,
+        startDestination = Route.Splash,
     ) {
-        composable<Splash> {
+        composable<Route.Splash> {
             SplashScreen {
-                navController.navigate(Onboarding)
+                navController.navigate(Route.Onboarding)
             }
         }
-
-        composable<Onboarding> {
+        composable<Route.Onboarding> {
             OnboardingScreen {
             }
         }
+        authGraph()
+        mainGraph()
+    }
+}
 
-        composable<Main> {
+fun NavGraphBuilder.mainGraph() {
+    navigation<Route.Main>(startDestination = Route.Home) {
+        composable<Route.Home> {
+            HomeScreen()
+        }
+        composable<Route.Search> {
+            SearchScreen()
+        }
+        composable<Route.Favorite> {
+            FavoriteScreen()
+        }
+        composable<Route.Profile> {
+            ProfileScreen()
+        }
+    }
+}
+
+fun NavGraphBuilder.authGraph() {
+    navigation<Auth>(startDestination = SignIn) {
+        composable<SignIn> {
+        }
+        composable<SignUp> {
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/me/dungngminh/lets_blog_kmp/presentation/main/profile/ProfileScreen.kt
@@ -0,0 +1,22 @@
+package me.dungngminh.lets_blog_kmp.presentation.main.profile
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier) {
+        Box(
+            modifier = Modifier.padding(it).fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text("ProfileScreen")
+        }
+    }
+}
