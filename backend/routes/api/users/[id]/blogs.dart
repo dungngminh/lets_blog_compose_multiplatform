79b03249d4923f3aa41188b2c348b58a3d4d274c
dungngminh <63831488+dungngminh@users.noreply.github.com>
2025-01-03 22:11:08 +0700

    backend: change get user blog response

--- backend/routes/api/users/[id]/blogs.dart
+++ backend/routes/api/users/[id]/blogs.dart
@@ -1,7 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:stormberry/stormberry.dart';
 import 'package:very_good_blog_app_backend/dtos/response/base_response_data.dart';
-import 'package:very_good_blog_app_backend/dtos/response/users/blogs/get_user_blog_response.dart';
+import 'package:very_good_blog_app_backend/dtos/response/blogs/get_blog_response.dart';
 import 'package:very_good_blog_app_backend/models/blog.dart';
 
 /// @Allow(GET)
@@ -17,7 +17,7 @@ Future<Response> _onUsersByIdBlogsGet(RequestContext context, String id) {
       .read<Database>()
       .blogs
       .queryBlogs(QueryParams(where: 'creator_id=@id', values: {'id': id}))
-      .then((views) => views.map(GetUserBlogResponse.fromView))
+      .then((views) => views.map(GetBlogResponse.fromView))
       .then<Response>((res) => OkResponse(res.map((e) => e.toJson()).toList()))
       .onError((e, _) => InternalServerErrorResponse(e.toString()))
       .whenComplete(() => context.read<Database>().close());
